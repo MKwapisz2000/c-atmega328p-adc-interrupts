dab06808430f70eb4fcceb0749c5d66a8202233c
MKwapisz <164620906+MKwapisz2000@users.noreply.github.com>
2025-02-13 11:47:00 +0100

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,120 @@
+//Użycie ADC w przerwaniach zewnętrznych, przy użyciu switcha,
+//bez użycia trigger source
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void ExternalInterupts_init()
+{
+  //failling edge
+  EICRA |= (1<<ISC01);
+  EIMSK |= (1<<INT0);
+
+  //wlaczenie przerwań 
+  sei();
+}
+
+void ADC_init()
+{
+  //zasilanie
+  ADMUX |= (1<<REFS0);
+  ADMUX &= ~(1<<REFS1);
+
+  //division factor
+  ADCSRA |= (1<<ADPS0);
+  ADCSRA |= (1<<ADPS1);
+  ADCSRA |= (1<<ADPS2);
+
+  //kanał A0
+  ADMUX &= ~(1<<MUX0);
+  ADMUX &= ~(1<<MUX1);
+  ADMUX &= ~(1<<MUX2);
+  ADMUX &= ~(1<<MUX3);
+
+  //włączenie przerwan
+  ADCSRA |= (1<<ADIE);
+
+  //auto trigger
+  //ADCSRA |= (1<<ADATE);
+
+  //wybranie trybu przerwań - na external
+  //ADCSRB |= (1<<ADTS1);
+  
+  //włączenie ADC
+  ADCSRA |= (1<<ADEN);
+
+  ADCSRA |= (1 << ADSC);
+}
+
+int channelA0()
+{
+  while(ADCSRA & (1<<ADSC))
+  {}
+
+  //zwracanie zmierzonego napięcia
+  return ADC;
+}
+
+int main()
+{
+  ADC_init();
+
+  //diody
+  DDRD |= (1<<PD5);
+  DDRD |= (1<<PD3);
+  DDRD |= (1<<PD4);
+
+  PORTD &= ~(1<<PD5);
+  PORTD &= ~(1<<PD3);
+  PORTD &= ~(1<<PD4);
+
+  //switch - pull-up
+  DDRD &= ~(1<<PD2);
+  PORTD |= (1<<PD2);
+
+  ExternalInterupts_init();
+  
+  while(1)
+  {
+   
+
+  }
+
+  return 0;
+}
+
+ISR(ADC_vect)
+{
+  int resultA0 = channelA0();
+  float napiecieA0 = resultA0*5.0/1023.0;
+    
+    if(napiecieA0 > 1.0)
+    {
+      PORTD |= (1<<PD5);
+    }
+    else
+    {
+      PORTD &= ~(1<<PD5);
+    }
+  
+   
+    if(napiecieA0 > 2.5)
+    {
+      PORTD |= (1<<PD4);
+    }
+    else
+    {
+      PORTD &= ~(1<<PD4);
+    }
+   
+    
+    if(napiecieA0 > 4.0)
+    {
+      PORTD |= (1<<PD3);
+    }
+    else
+    {
+      PORTD &= ~(1<<PD3);
+    }
+
+}
